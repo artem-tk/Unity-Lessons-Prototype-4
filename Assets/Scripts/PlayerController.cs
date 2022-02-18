2bff89d9ad075f58d4088b00028e40715c71c156
artem-tk <artem.tk24@gmail.com>
2022-02-18 04:14:46 +0300

    Powerup pt.1

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,7 +7,10 @@ public class PlayerController : MonoBehaviour
     private Rigidbody PlayerRB;
     private GameObject FocalPoint;
 
+    public bool Poweruped = false;
+
     public float Speed = 5.0f;
+    private float PowerupStrength = 15.0f;
 
     void Start()
     {
@@ -20,5 +23,27 @@ public class PlayerController : MonoBehaviour
         float ForwardInput = Input.GetAxis("Vertical");
 
         PlayerRB.AddForce(FocalPoint.transform.forward * Speed * ForwardInput);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup"))
+        {
+            Poweruped = true;
+            Destroy(other.gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy") && Poweruped)
+        {
+            Rigidbody EnemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 AwayFromPlayer = (collision.gameObject.transform.position - transform.position);
+
+            EnemyRigidbody.AddForce(AwayFromPlayer * PowerupStrength, ForceMode.Impulse);
+
+            Debug.Log("U collide with " + collision.gameObject.name + " while powerup was " + Poweruped);   
+        }
     }
 }
